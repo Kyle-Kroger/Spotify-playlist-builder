6f3b9ba4d69ab9584421ca1db7943db63cf1ac3d
Kyle Kroger <kyle.s.kroger@gmail.com>
2023-06-28 20:25:40 -0500

    del artist and artist list

--- components/musicInfo/index.ts
+++ components/musicInfo/index.ts
@@ -1,10 +1,6 @@
 export { default as TrackList } from "./TrackList";
 export { default as Track } from "./Track";
 export { default as ArtistPage } from "./ArtistPage";
-export { default as ArtistList } from "./ArtistList";
-export { default as Artist } from "./Artist";
-export { default as Album } from "./Album";
-export { default as AlbumList } from "./AlbumList";
 export { default as AlbumPage } from "./AlbumPage";
 export { default as MusicItem } from "./MusicItem";
 export { default as MusicHeadingItem } from "./MusicHeadingItem";
